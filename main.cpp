680e34d03f9e34a90ed1558f913b498c85d0d7ae
Mikael-ai <mikikusmikikus@gmail.com>
2023-03-12 17:49:41 +0300

    Added dark theme

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "mangaviewer.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    qApp->setPalette(darkPalette);
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+
     MangaViewer w;
     w.show();
     return a.exec();
